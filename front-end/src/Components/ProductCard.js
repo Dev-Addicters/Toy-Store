da7d6a4becd922ced2c9f21c28a04ab2e15514dc
Alexander <alexanderl96@yahoo.com>
2021-07-21 13:06:36 -0400

    adding img at home'

--- front-end/src/Components/ProductCard.js
+++ front-end/src/Components/ProductCard.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import newProduct from './images/new.png'
+import { Link } from "react-router-dom"
+
+
 
 export default function ProductCard ({ product }) {
+  
+
   return (
     <div className='productCard'>
-      {product.isnew && <img src={newProduct} alt='New' />}
-      <img src={product.image} alt={product.name} />
+      {product.is_new && <img src={newProduct} alt='New' />}
+       <img src={product.image} alt={product.name} />
       <p>
         {product.category}
         <br />
         <b>{product.name}</b>
         <br />$ {product.price}
       </p>
-      <button>Add to Cart</button>
+      <Link to={`/`}><button>Add to Cart</button></Link>
     </div>
   )
 }
--- front-end/src/Pages/Show.js
+++ front-end/src/Pages/Show.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ItemsApi from '../Components/ItemsApi'
+import { useState } from 'react'
+import { useParams } from 'react-router'
 
-export default function Show() {
+export default function Show( { product }) {
+    const {id} = useParams()
+    const [products]= useState(product[id])
     return (
-        <div>
-            <h1>Show</h1>
-            <ItemsApi />
-        </div>
+        <div className='productCard'>
+            <ItemsApi products={products} id={id}/>
+       </div>
     )
 }
